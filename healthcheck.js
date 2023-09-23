72d0cd40f2f20556365c503a328eb4f815bbb36e
wajeht <58354193+wajeht@users.noreply.github.com>
2023-09-22 19:40:51 -0500

    fix: does this work

--- healthcheck.js
+++ healthcheck.js
@@ -8,27 +8,36 @@ const options = {
 };
 
 request(options, (res) => {
-		let body = '';
+	let body = '';
 
-		res.on('data', (chunk) => {
-			body += chunk;
-		});
+	res.on('data', (chunk) => {
+		body += chunk;
+	});
 
-		res.on('end', () => {
-			try {
-				const response = JSON.parse(body);
-				if (response.healthy === true) {
-					process.exit(0);
+	res.on('end', () => {
+		try {
+			// Ensure the response is valid JSON before parsing
+			const isValidJSON = () => {
+				try {
+					JSON.parse(body);
+					return true;
+				} catch {
+					return false;
 				}
+			};
 
+			if (isValidJSON() && JSON.parse(body).healthy === true) {
+				process.exit(0);
+			} else {
 				console.log('Unhealthy response received: ', body);
 				process.exit(1);
-			} catch (err) {
-				console.log('Error parsing JSON response body: ', err);
-				process.exit(1);
 			}
-		});
-	})
+		} catch (err) {
+			console.log('Error parsing JSON response body: ', err);
+			process.exit(1);
+		}
+	});
+})
 	.on('error', (err) => {
 		console.log('Error: ', err);
 		process.exit(1);
